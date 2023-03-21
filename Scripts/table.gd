8132fbcdb154c3452ca3daf22321c8879839973f
Alexandre Massaro <alexandremassaro@gmail.com>
2023-03-21 14:11:23 -0300

    Blocking a Game Arena

--- Scripts/table.gd
+++ Scripts/table.gd
@@ -2,11 +2,11 @@ extends Node3D
 
 var screen_size : Vector2i = Vector2i.ZERO
 
-@onready var gaming_space : Control = get_node("GamingSpace")
+#@onready var gaming_space : Control = get_node("GamingSpace")
 
 func _ready():
-	screen_size = get_viewport().size
-	gaming_space.set_size(screen_size)
+#	screen_size = get_viewport().size
+#	gaming_space.set_size(screen_size)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
